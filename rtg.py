6772aa6d01a7c6c40a07d2ecb778c8dde570e14e
Howon Lee <howon@howonlee.com>
2015-04-05 21:09:46 -0700

    rtg almost done

--- rtg.py
+++ rtg.py
@@ -86,9 +86,29 @@ def rtg_iu(k, qs, p, W):
     words = [iu_gen_word(weighted_choice_fn, p) for word in xrange(W)]
     return words
 
+
+def create_qmat(qs, p):
+    qs.append(p)
+    qs_norm = np.atleast_2d(np.array(qs) / np.linalg.norm(np.array(qs)))
+    len_qmat = qs_norm.shape[1] - 1 #the index of qmat that indicates space
+    q_mat = qs_norm * qs_norm.T
+    return q_mat
+
+def q_gen(k_set, qmat):
+    """
+    Corresponds to SelectNodeLabels in the RTG paper
+    """
+    l1, l2 = "", ""
+    l1_term, l2_term = False, False
+    while ((not l1_term) and (not l2_term)):
+        rand = random.random()
+        #stuff stuff stuff
+
 def rtg(k, qs, p, W, beta):
+    k_set = map(str, range(k))
+    qmat = create_qmat(qs, p)
     pass
 
 if __name__ == "__main__":
-    #words = rtg_iu(5, [0.1,0.1,0.1,0.3,0.4], 0.15, 1000)
+    words = rtg(10, range(2,12), 0.5, 1000, 0.9)
     #print words_to_graph(words)
