3243a0a349c7ddae817223e4300bcda842a4a0b4
Howon Lee <howon@howonlee.com>
2015-04-05 20:56:55 -0700

    iu successfully exists

--- rtg.py
+++ rtg.py
@@ -1,6 +1,7 @@
 import random
 import itertools
 import collections
+import numpy as np
 import networkx as nx
 import bisect
 
@@ -41,25 +42,53 @@ def rtg_ie(k, p, W):
     words = [ie_gen_word(k_set, p) for word in xrange(W)]
     return words
 
-def weighted_choice(choices, ps):
+def weighted_choice_fac(choices, ps):
+    """
+    weighted choice factory
+    I could also have futzed with decorator but this is easier
+    """
     total = 0
     cum_ps = []
     for p in ps:
-        total += w
+        total += p
         cum_ps.append(total)
-    x = random() * total
-    return values[bisect.bisect(cum_ps, x)]
+    def weighted_choice():
+        x = random.random() * total
+        return choices[bisect.bisect(cum_ps, x)]
+    return weighted_choice
 
 
-def iu_gen_word(k_set, qs, p):
-    pass
+def iu_gen_word(weighted_choice_fn, p):
+    word = ""
+    word = word + weighted_choice_fn() + ","
+    while random.random() > p:
+        word = word + weighted_choice_fn() + ","
+    return word
 
-def rtg_iu(k, p):
-    pass
+def adjust_matrix_homophily(mat, beta):
+    #assumes a square matrix
+    #also assumes 0 < beta < 1
+    mat_shape = mat.shape
+    print mat_shape
+    for x in xrange(mat_shape[0]):
+        row_sum = 0
+        for y in xrange(mat_shape[1]):
+            if x != y:
+                temp = mat[x, y] * beta
+                row_sum += mat[x,y] - (temp)
+                mat[x, y] = temp
+        mat[x,x] += row_sum
+    return mat
 
-def rtg(k, q, w, beta):
+def rtg_iu(k, qs, p, W):
+    k_set = map(str, range(k))
+    weighted_choice_fn = weighted_choice_fac(k_set, qs)
+    words = [iu_gen_word(weighted_choice_fn, p) for word in xrange(W)]
+    return words
+
+def rtg(k, qs, p, W, beta):
     pass
 
 if __name__ == "__main__":
-    words = rtg_ie(5, 0.15, 1000)
-    words_to_graph(words)
+    #words = rtg_iu(5, [0.1,0.1,0.1,0.3,0.4], 0.15, 1000)
+    #print words_to_graph(words)
